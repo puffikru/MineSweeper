95200afe01fd3afaa6f0246b46f6a8914034ea50
Igor Bulakh <igorbulakh1987@yandex.ru>
2024-12-16 16:27:01 +0300

    Add getch function and check running OS

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -1,8 +1,53 @@
 #include "minesweeper.h"
 
+#include <iostream>
+
+// Board :: Board() 
+// {
+    
+// }
+
+Board :: Board(size_t size, size_t mines)
+: size_(size), mines_(mines) 
+{
+    board.resize(size, std::vector<Cell>(size));
+}
+
+void Board :: Print() const
+{
+    std::cout << "#########################" << "\n";
+        for (size_t i = 0; i < board.size(); ++i)
+        {
+            for (size_t j = 0; j < board[i].size(); ++j)
+            {
+                if (board[i][j].state != CellState::OPENED)
+                {
+                    std::cout << "0";
+                }
+            }
+            std::cout << '\n';
+        }
+        std::cout << "#########################" << '\n';
+}
+
+Game :: Game(size_t size, size_t mines)
+: board(size, mines) {}
+
+void Game :: Start() const
+{
+
+}
+
 int main()
 {
     Game game(50, 10);
     game.Start();
+
+    #if defined(_WIN32) || defined(_WIN64)
+        std::cout << "Running on Windows." << std::endl;
+    #elif defined(__APPLE__) || defined(__MACH__)
+        std::cout << "Running on macOS." << std::endl;
+    #endif
+
     return 0;
 }
--- minesweeper.h
+++ minesweeper.h
@@ -1,6 +1,33 @@
 #pragma once
 
+#include <termios.h>
+#include <unistd.h>
 #include <vector>
+
+#if defined(__APPLE__) || defined(__MACH__)
+char getch() {
+    struct termios oldt, newt;
+    char ch;
+
+    // Получаем текущие настройки терминала
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+
+    // Отключаем буферизацию и ввод по нажатию Enter
+    newt.c_lflag &= ~(ICANON | ECHO);
+
+    // Устанавливаем новые настройки терминала
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+    // Считываем символ
+    ch = getchar();
+
+    // Восстанавливаем старые настройки терминала
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+
+    return ch;
+}
+#endif
 
 enum CellState
 {
@@ -19,22 +46,26 @@ struct Cell
 class Board
 {
 public:
-    Board();
 
-    Board(int size, int mines) : size_(size), mines_(mines) {};
+
+    Board(size_t size, size_t mines);
+
+    void Print() const;
     
 private:
-    int size_;
-    int mines_;
+    const size_t size_;
+    const size_t mines_;
     std::vector<std::vector<Cell>> board;
 };
 
 class Game
 {
 public:
-    Game(int size, int mines) : board(size, mines) {}
+    Game(size_t size, size_t mines);
 
-    void Start();
+    void Start() const;
+
+
 
 private:
     Board board;
